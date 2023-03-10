b303dc6c5862cbdc004f8565484a3e89f09a355c
Coossme <garcos972@gmail.com>
2023-03-10 21:05:42 +0100

    Testeo3Calculadora

--- CalculadoraTDD/src/OperacionesTDD.java
+++ CalculadoraTDD/src/OperacionesTDD.java
@@ -4,15 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OperacionesTDD {
     public int operacion (String numero) {
+        if (numero.substring(0, 1).equals(",") || numero.substring(numero.length() - 1, numero.length()).equals(",")){
+            return -1;
+        }
         if (numero.equals("")) {
             return 0;
         }
         String coma[] = numero.split(",");
         int suma=0;
         for (int i = 0; i < coma.length; i++) {
-            int total = Integer.valueOf(coma[i]); ;
+            if (coma[i].equals("")){
+                return -1;
+            }
+            int total = Integer.valueOf(coma[i]);
             suma = suma + total;
+
+
         }
+
         return suma;
     }
 
--- CalculadoraTDD/test/OperacionesTDDTest.java
+++ CalculadoraTDD/test/OperacionesTDDTest.java
@@ -54,4 +54,18 @@ class OperacionesTDDTest {
         System.out.println(numero);
 
     }
+
+    @Test
+    void testeo1mas2masNadaDevuelve4() {
+        //Arrange
+        OperacionesTDD calculadora = new OperacionesTDD();
+
+        //Act
+        int numero = calculadora.operacion("1,2,");
+
+        //Assert
+        assertEquals(-1, numero);
+        System.out.println(numero);
+
+    }
 }
