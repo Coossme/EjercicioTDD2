42e078556dba8ab8f7198d080f7b877c249c1d86
Coossme <garcos972@gmail.com>
2023-03-10 21:14:55 +0100

    Testeo3Calculadora

--- CalculadoraTDD/src/OperacionesTDD.java
+++ CalculadoraTDD/src/OperacionesTDD.java
@@ -4,11 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OperacionesTDD {
     public int operacion (String numero) {
+        if (numero.length()==0) {
+            return 0;
+        }
         if (numero.substring(0, 1).equals(",") || numero.substring(numero.length() - 1, numero.length()).equals(",")){
             return -1;
-        }
-        if (numero.equals("")) {
-            return 0;
         }
         String coma[] = numero.split(",");
         int suma=0;
@@ -17,6 +17,10 @@ public class OperacionesTDD {
                 return -1;
             }
             int total = Integer.valueOf(coma[i]);
+            if (total < 0){
+                System.out.println("Número negativo no permitido");
+                return -1;
+            }
             suma = suma + total;
         }
 
--- CalculadoraTDD/test/OperacionesTDDTest.java
+++ CalculadoraTDD/test/OperacionesTDDTest.java
@@ -67,4 +67,16 @@ class OperacionesTDDTest {
         assertEquals(-1, numero);
         System.out.println(numero);
     }
+    @Test
+    void testeo1mas2masMenos2Devuelve4() {
+        //Arrange
+        OperacionesTDD calculadora = new OperacionesTDD();
+
+        //Act
+        int numero = calculadora.operacion("1,2,-2");
+
+        //Assert
+        assertEquals(-1, numero);
+        System.out.println(numero);
+    }
 }
